aaba8485a77bd88d5c6e91f651c231c653c52164
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-11-08 20:26:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,8 +10,8 @@ class script(object):
 - ᴇᴠᴀ ᴍᴀʀɪᴀ ɪꜱ ᴀ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ. 
 - ꜱᴏᴜʀᴄᴇ - https://t.me/mazhatthullikal  
 
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+<b>ᴅᴇᴠꜱ:</b>
+- <a href=https://t.me/mazhatthullikal>ᴍᴜɴɴɪᴘᴏᴘᴢ</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
@@ -36,10 +36,10 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/mazhattullikal)</code>
 
 <b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
+<code>[Button Text](buttonalert:type ur text)</code>"""
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
 <b>NOTE:</b>
