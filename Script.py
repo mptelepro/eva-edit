ea6216e5547f4e83ae0ac3e30999fc0f656d703a
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-11-08 20:19:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,18 +1,14 @@
 class script(object):
     START_TXT = """ʜᴀɪ {},
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 💥ᴍᴏᴠɪᴇꜱ ɢʀᴏᴜᴘ ᴍᴀɴᴜᴀʟ ɪᴍʙᴅ ꜰɪʟᴛᴇʀ ʙᴏᴛ 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+    HELP_TXT = """ഹലോ{}
+നിങ്ങൾക്ക് എന്നെ ഉപയോഗിക്കേണ്ട രീതികൾ അറിയാനുള്ള കമാണ്ടുകൾ.."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/bigmoviesworld>Team Kochukkallan</a>
+✯ᴍᴜɴɴɪᴘᴏᴘᴢ"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- ᴇᴠᴀ ᴍᴀʀɪᴀ ɪꜱ ᴀ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ. 
+- ꜱᴏᴜʀᴄᴇ - https://t.me/mazhatthullikal  
 
 <b>DEVS:</b>
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
